2012ec3f17013a6ac679240fce9d14d968407008
Brian Lindsey <brianl9864@gmail.com>
2018-02-22 14:21:02 -0500

    add /auth/jwt/keepAlive and /auth/jwt/setAcf endpoints

--- src/main/java/org/pulseus/auth/service/rest/controller/JWTController.java
+++ src/main/java/org/pulseus/auth/service/rest/controller/JWTController.java
@@ -4,6 +4,9 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.Role;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 
 import java.util.ArrayList;
@@ -21,6 +24,8 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.pulseus.auth.portlet.security.filter.PortletSecurityFilter;
 import org.springframework.security.crypto.codec.Base64;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +41,10 @@ public class JWTController {
 	Configuration configuration = ConfigurationFactoryUtil.getConfiguration(PortalClassLoaderUtil.getClassLoader(), "portlet");
 	
 	private static final Logger log = LogManager.getLogger(JWTController.class);
+	
+	private static final String ORGANIZATIONS = "Orgs";
+	private static final String AUTHORITIES = "Authorities";
+	private static final String IDENTITY = "Identity";
 	
     @RequestMapping(value = "/auth/jwt", method = RequestMethod.GET, produces = "application/json")
     public String getJwt(HttpServletRequest request) throws PortalException {
@@ -63,9 +72,9 @@ public class JWTController {
 			}
 		}
 		
-		jwtClaims.put("Orgs", orgs);
-		jwtClaims.put("Identity", identity);
-		jwtClaims.put("Authorities", jwtAuthorities);
+		jwtClaims.put(ORGANIZATIONS, orgs);
+		jwtClaims.put(IDENTITY, identity);
+		jwtClaims.put(AUTHORITIES, jwtAuthorities);
 		
 		Calendar c = Calendar.getInstance();
 		c.add(Calendar.HOUR, 1);  // number of days to add
@@ -73,7 +82,69 @@ public class JWTController {
 
 		
 		String jwt = Jwts.builder()
-    	  .setSubject(userLoggedIn.getEmailAddress())
+    	  .setExpiration(expires)
+    	  .addClaims(jwtClaims)
+    	  .signWith(SignatureAlgorithm.HS512, Base64.decode((configuration.get("jwt.key").getBytes()))).compact();
+		
+        String jwtJSON = "{\"token\": \""+ jwt +"\"}";
+        
+        return jwtJSON;
+    }
+    
+    @RequestMapping(value = "/auth/jwt/setAcf", method = RequestMethod.POST, produces = "application/json")
+    public String setAcf(@RequestHeader(value="Authorization") String authorization, @RequestBody String acf) throws PortalException {
+    	String jwt = null;
+        String oldJwt = authorization.split(" ")[1];
+
+        // Parse old Jwt
+        Jwt<Header, Claims> claims = Jwts.parser().setSigningKey(Base64.decode(configuration.get("jwt.key").getBytes())).parse(oldJwt);
+        
+        List<String> authorityInfo = (List<String>) claims.getBody().get("Authorities");
+        List<String> identityInfo = (List<String>) claims.getBody().get("Identity");
+        HashMap<String, Long> orgInfo = (HashMap<String, Long>) claims.getBody().get("Orgs");
+        identityInfo.add(acf);
+        
+        Map<String, Object> jwtClaims = new HashMap<String, Object>();
+        jwtClaims.put("Authorities", authorityInfo);
+        jwtClaims.put("Identity", identityInfo);
+        jwtClaims.put("Orgs", orgInfo);
+		
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.HOUR, 1);  // number of days to add
+		Date expires = c.getTime();
+
+		jwt = Jwts.builder()
+    	  .setExpiration(expires)
+    	  .addClaims(jwtClaims)
+    	  .signWith(SignatureAlgorithm.HS512, Base64.decode((configuration.get("jwt.key").getBytes()))).compact();
+		
+        String jwtJSON = "{\"token\": \""+ jwt +"\"}";
+        
+        return jwtJSON;
+    }
+    
+    @RequestMapping(value = "/auth/jwt/keepAlive", method = RequestMethod.POST, produces = "application/json")
+    public String setKeepAlive(@RequestHeader(value="Authorization") String authorization) throws PortalException {
+    	String jwt = null;
+        String oldJwt = authorization.split(" ")[1];
+
+        // Parse old Jwt
+        Jwt<Header, Claims> claims = Jwts.parser().setSigningKey(Base64.decode(configuration.get("jwt.key").getBytes())).parse(oldJwt);
+        
+        List<String> authorityInfo = (List<String>) claims.getBody().get("Authorities");
+        List<String> identityInfo = (List<String>) claims.getBody().get("Identity");
+        HashMap<String, Long> orgInfo = (HashMap<String, Long>) claims.getBody().get("Orgs");
+        
+        Map<String, Object> jwtClaims = new HashMap<String, Object>();
+        jwtClaims.put("Authorities", authorityInfo);
+        jwtClaims.put("Identity", identityInfo);
+        jwtClaims.put("Orgs", orgInfo);
+		
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.HOUR, 1);  // number of days to add
+		Date expires = c.getTime();
+
+		jwt = Jwts.builder()
     	  .setExpiration(expires)
     	  .addClaims(jwtClaims)
     	  .signWith(SignatureAlgorithm.HS512, Base64.decode((configuration.get("jwt.key").getBytes()))).compact();
