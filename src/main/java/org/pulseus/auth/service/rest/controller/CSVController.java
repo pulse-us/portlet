ada0a8fb7e59b8f024110b774de16d33d27ffbee
INTRANET\ssavarala <ssavarala@ainq.com>
2018-03-05 13:58:17 -0500

    adding logs

--- src/main/java/org/pulseus/auth/service/rest/controller/CSVController.java
+++ src/main/java/org/pulseus/auth/service/rest/controller/CSVController.java
@@ -2,6 +2,7 @@ package org.pulseus.auth.service.rest.controller;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.UUID;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.liferay.portal.kernel.configuration.Configuration;
-import com.liferay.portal.kernel.configuration.ConfigurationFactoryUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
@@ -22,84 +21,95 @@ import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.StringPool;
 
 @RestController
 public class CSVController {
-	Configuration configuration = ConfigurationFactoryUtil.getConfiguration(PortalClassLoaderUtil.getClassLoader(), "portlet");
 
 	private static final Logger log = LogManager.getLogger(JWTController.class);
-	
+
 	@RequestMapping(value = "/addusers", method = RequestMethod.GET)
 	public void addusers() throws Exception {
-		System.out.println("adding users");
+		log.info("adding users");
 		ServiceContext serviceContext = new ServiceContext();
 		Long companyId = CompanyThreadLocal.getCompanyId();	
-		
+
 
 
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
+		
 
 		br = new BufferedReader(new FileReader("/opt/pulse/users.csv"));
-		while ((line = br.readLine()) != null) {
+		
+		long creatorUserId = 0;
+		boolean autoPassword = false;
+		boolean autoScreenName = false;
+		long facebookId = 0;
+		String openId = StringPool.BLANK;
+		Locale locale = serviceContext.getLocale();
+		String middleName = StringPool.BLANK;
+		int prefixId = 0;
+		int suffixId = 0;
+		boolean male = true;
+		int birthdayMonth = Calendar.JANUARY;
+		int birthdayDay = 1;
+		int birthdayYear = 1970;
+		String jobTitle = StringPool.BLANK;
+		long[] groupIds = null;
+		long[] userGroupIds = null;
+		boolean sendEmail = false;
+		
+		while (true) {
+			try {
+				if ((line = br.readLine()) != null) {
+					// use comma as separator
+					String[] value = line.split(cvsSplitBy);
+					Role role = RoleLocalServiceUtil.getRole(companyId, value[5]);
+					Long roleId = 	role.getRoleId();
 
-			// use comma as separator
-			String[] value = line.split(cvsSplitBy);
-			Role role = RoleLocalServiceUtil.getRole(companyId, value[5]);
-			Long roleId = 	role.getRoleId();
-			
-			Organization org1 = OrganizationLocalServiceUtil.getOrganization(companyId, value[6]);
-			Long orgId1 = org1.getOrganizationId();
+					Organization org1 = OrganizationLocalServiceUtil.getOrganization(companyId, value[6]);
+					Long orgId1 = org1.getOrganizationId();
 
-			Organization org2 = OrganizationLocalServiceUtil.getOrganization(companyId, value[7]);
-			Long orgId2 = org2.getOrganizationId();
-			
-			long creatorUserId = 0;
-			boolean autoPassword = false;
-			boolean autoScreenName = false;
-			long facebookId = 0;
-			String openId = StringPool.BLANK;
-			Locale locale = serviceContext.getLocale();
-			String middleName = StringPool.BLANK;
-			int prefixId = 0;
-			int suffixId = 0;
-			boolean male = true;
-			int birthdayMonth = Calendar.JANUARY;
-			int birthdayDay = 1;
-			int birthdayYear = 1970;
-			String jobTitle = StringPool.BLANK;
-			long[] groupIds = null;
-			long[] organizationIds = {orgId1,orgId2};
-			long[] roleIds = {roleId};
-			long[] userGroupIds = null;
-			boolean sendEmail = false;
+					Organization org2 = OrganizationLocalServiceUtil.getOrganization(companyId, value[7]);
+					Long orgId2 = org2.getOrganizationId();
 
-			String screenName = value[0];
-			String emailAddress = value[1];
-			String firstName = value[2];
-			String lastName = value[3];
-			String password1 = value[4];
-			String password2 = value[4];
+					long[] organizationIds = {orgId1,orgId2};
+					long[] roleIds = {roleId};
 
-			String uuid = UUID.randomUUID().toString().replace("-", "");
-			serviceContext.setUuid(uuid);
+					String screenName = value[0];
+					String emailAddress = value[1];
+					String firstName = value[2];
+					String lastName = value[3];
+					String password1 = value[4];
+					String password2 = value[4];
 
-			User user = UserLocalServiceUtil.addUser(
-					creatorUserId, companyId, autoPassword, password1, password2,
-					autoScreenName, screenName, emailAddress, facebookId, openId,
-					locale, firstName, middleName, lastName, prefixId, suffixId, male,
-					birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
-					organizationIds, roleIds, userGroupIds, sendEmail, serviceContext);
+					String uuid = UUID.randomUUID().toString().replace("-", "");
+					serviceContext.setUuid(uuid);
 
-			user = UserLocalServiceUtil.updateEmailAddressVerified(user.getUserId(), true);
-			user = UserLocalServiceUtil.updatePasswordReset(user.getUserId(), false);
+					User user = UserLocalServiceUtil.addUser(
+							creatorUserId, companyId, autoPassword, password1, password2,
+							autoScreenName, screenName, emailAddress, facebookId, openId,
+							locale, firstName, middleName, lastName, prefixId, suffixId, male,
+							birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+							organizationIds, roleIds, userGroupIds, sendEmail, serviceContext);
 
-			System.out.println("user added");
+					user = UserLocalServiceUtil.updateEmailAddressVerified(user.getUserId(), true);
+					user = UserLocalServiceUtil.updatePasswordReset(user.getUserId(), false);
+
+					log.info("user added "+value[0]);
+
+				}
+			}
+
+			catch(Exception e) {
+				log.info(e.getMessage());
+				log.info("The line causing error is " + Arrays.toString(line.split(cvsSplitBy)));
+				continue;
+			}
+
 		}
 
-		br.close();
 	}
 }
